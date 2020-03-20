bcbab6a7922c17ebd35263ae28d2f7432dcb5341
elzurdo <eyalkazin@gmail.com>
2020-03-20 13:45:55 +0000

    using dictionaries, work in progress ...

--- protocols/_example_dummy_scripts/rna_extraction.py
+++ protocols/_example_dummy_scripts/rna_extraction.py
@@ -4,19 +4,39 @@
 # to run from bash command line:
 # > opentrons_simulate rna_extraction.py
 
+# based on nucleic_acid_extraction.ot2.py from https://protocol-delivery.protocols.opentrons.com/protocol/1584
+
+from collections import OrderedDict
 from opentrons import labware, instruments, modules, robot
 
 # TODO
 # figure out how to get LYSIS_BUFFER_NEWTIP='never' to work!
 # figure out how to tell machine to stop, remember where it is and to continue after command
 
-LYSIS_BUFFER_VOLUME = 240  # μl
-LYSIS_BUFFER_NEWTIP = 'never' # other options: 'never', 'once'
-LYSIS_MIX_REPETITIONS = 15
+# comments
+# transfer_volume is in μl
+# new_tip options: 'always', 'never', 'once'
+
+NEW_TIP = 'never'
+MIX_REPETITIONS = 15
+
+reagents = OrderedDict()
+# Add 240 μl of lysis buffer, seal and shake at RT at 1400 rpm for 5 min
+reagents['lysis_buffer'] = {'well': 'A1', 'transfer_volume': 240, 'mix_volume': 240, 'mix_repetitions': MIX_REPETITIONS,
+                            'new_tip': NEW_TIP}
+# Add 320 μl of isopropanol, seal and shake at RT at 1400 rpm for 5 min
+reagents['isopropanol_320'] = {'well': 'A2', 'transfer_volume': 300, 'mix_volume': 300, 'mix_repetitions': MIX_REPETITIONS,
+                            'new_tip': NEW_TIP}
+# Add 40 μl of silica-coated magnetic beads (BOMB protocol #2.1, 1:10 diluted from stock), seal and shake at RT at 1400 rpm for 5 min
+reagents['magnetic_beads'] = {'well': 'A3', 'transfer_volume': 40, 'mix_volume': 40, 'mix_repetitions': MIX_REPETITIONS,
+                            'new_tip': NEW_TIP}
+# Remove the plate from the magnetic stand and add 400 μl isopropanol. Shake at RT at 1400 rpm for 2 min
+reagents['isopropanol_400'] = {'well': 'A2', 'transfer_volume': 300, 'mix_volume': 300, 'mix_repetitions': MIX_REPETITIONS,
+                            'new_tip': NEW_TIP}
 
 metadata = {
     'protocolName': 'RNA Extraction v0.1',
-    'author': 'Neil MacKenzie, Eyal Kazin <protocols@opentrons.com>',
+    'author': 'Neil MacKenzie, Eyal Kazin <eyalkazin@gmail.com>',
     'source': 'Testing' #'Custom Protocol Request'
 }
 
@@ -52,16 +72,26 @@ m300 = instruments.P300_Multi(mount='right', tip_racks=tips)
 
 
 # reagent setup
-lysis_buffer = trough.wells('A1')
-isopropanol = trough.wells('A2')
-magnetic_bead = trough.wells('A3')   # e.g, silica-coated magnetic beads (BOMB protocol #2.1, 1:10 diluted from stock)
-ethanol_80percent = trough.wells('A4')
-dnaseI_reaction_mix = trough.wells('A5')  # enzyme that removes DNA
-rna_binding_buffer = trough.wells('A6')
-nuclease_free_water = trough.wells('A7')
-liquid_waste = trough.wells('A12')  #  elusion waste
 
-#def transfer_and_mix():
+for reagent_name in reagents:
+    reagents[reagent_name]['setup'] = trough.wells('A1')
+
+
+#lysis_buffer = trough.wells('A1')
+# isopropanol = trough.wells('A2')
+# magnetic_bead = trough.wells('A3')   # e.g, silica-coated magnetic beads (BOMB protocol #2.1, 1:10 diluted from stock)
+# ethanol_80percent = trough.wells('A4')
+# dnaseI_reaction_mix = trough.wells('A5')  # enzyme that removes DNA
+# rna_binding_buffer = trough.wells('A6')
+# nuclease_free_water = trough.wells('A7')
+# liquid_waste = trough.wells('A12')  #  elusion waste
+
+def transfer_and_mix(reagent, samples):
+    for s in samples:
+        m300.pick_up_tip()
+        m300.transfer(reagent['transfer_volume'], reagent['setup'], s, new_tip=reagent['new_tip'])
+        m300.mix(reagent['mix_repetitions'], reagent['mix_volume'], s)  # note that according to nucleic_acid_extration.ot2.py .mix volume differs from .transfer volume
+        m300.drop_tip()
 
 def run_custom_protocol(number_of_sample_columns: int = 12):
     if number_of_sample_columns > 12:
@@ -70,18 +100,21 @@ def run_custom_protocol(number_of_sample_columns: int = 12):
     samples = sample_plate.rows('A')[0:number_of_sample_columns]
 
     # transfer lysis and mix (bursting the cells open)
-    for s in samples:
-        m300.pick_up_tip()
-        m300.transfer(LYSIS_BUFFER_VOLUME, lysis_buffer, s, new_tip=LYSIS_BUFFER_NEWTIP)
-        m300.mix(LYSIS_MIX_REPETITIONS, LYSIS_BUFFER_VOLUME, s)  # note that according to nucleic_acid_extration.ot2.py .mix volume differs from .transfer volume
-        m300.drop_tip()
+    transfer_and_mix(reagents['lysis_buffer'], samples)
 
     # --- OpenTron does not have Seal and Shake modules ---
     # figure out how to tell machine to stop, remember where it is and to continue after command
-
     # transfer and mix isopropanol
 
+    transfer_and_mix(reagents['isopropanol_320'], samples)
+
+    transfer_and_mix(reagents['magnetic_beads'], samples)
+
+    transfer_and_mix(reagents['isopropanol_400'], samples)
 
 
 
-run_custom_protocol(**{'number_of_sample_columns': 12})
+
+
+
+run_custom_protocol(**{'number_of_sample_columns': 2})
